b3ca556cd9bfb1caada8d4f19149bc20f2b4eabd
LordBonny <bonny500@gmail.com>
2015-10-16 14:35:53 +0700

    first
    bonny

---
+++ platforms/ios/iOSRTCApp/Plugins/Unified-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Unified-Bridging-Header.h
+//  iOSRTCApp
+//
+//  Created by Tanet Pornsirirat on 10/15/2558 BE.
+//
+//
+
+// cordova-plugin-apple-watch "Apple Watch"
+#import "Watch-Bridge.h"
+
+// cordova-plugin-iosrtc
+#import "cordova-plugin-iosrtc-Bridging-Header.h"
---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,71 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+        "id": "cordova-plugin-camera.Camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+        "id": "cordova-plugin-camera.CameraPopoverOptions",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/Camera.js",
+        "id": "cordova-plugin-camera.camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/ios/CameraPopoverHandle.js",
+        "id": "cordova-plugin-camera.CameraPopoverHandle",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-iosrtc/dist/cordova-plugin-iosrtc.js",
+        "id": "cordova-plugin-iosrtc.Plugin",
+        "clobbers": [
+            "cordova.plugins.iosrtc"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
+        "id": "org.apache.cordova.console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/logger.js",
+        "id": "org.apache.cordova.console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-camera": "1.1.1-dev",
+    "cordova-plugin-crosswalk-webview": "1.4.0-dev",
+    "cordova-plugin-device": "1.0.1",
+    "cordova-plugin-iosrtc": "1.4.5",
+    "cordova-plugin-whitelist": "1.0.0",
+    "org.apache.cordova.console": "0.2.13"
+}
+// BOTTOM OF METADATA
+});
